f5fc14f797e35947d42af4f44a94f600761c8e45
Gabriel Fontana <gabrielf.junqueira74@gmail.com>
2022-03-28 19:19:30 -0300

    Migrando o projeto para o JavaFX 11

---
+++ database.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE IF NOT EXISTS `boxgym`;
+
+USE `boxgym`;
+
+CREATE TABLE `supplier` (
+  `supplierId` INT(11) NOT NULL AUTO_INCREMENT,
+  `companyRegistry` CHAR(14) NOT NULL,
+  `corporateName` VARCHAR(255) NOT NULL,
+  `tradeName` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NULL, 
+  `phone` VARCHAR(11) NULL, 
+  `zipCode` CHAR(8) NULL,
+  `address` VARCHAR(255) NULL, 
+  `addressComplement` VARCHAR(255) NULL, 
+  `district` VARCHAR(255) NULL, 
+  `city` VARCHAR(255) NULL, 
+  `federativeUnit` CHAR(2) NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`supplierId`),
+  UNIQUE KEY `supplierUnique` (`companyRegistry`) USING BTREE
+)
+
+CREATE TABLE `product` (
+  `productId` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `category` VARCHAR(255),
+  `description` TEXT,
+  `amount` INT(10) NOT NULL,
+  `minimumStock` INT(10) NOT NULL,
+  `costPrice` DECIMAL(10, 2) NOT NULL,
+  `sellingPrice` DECIMAL(10, 2) NOT NULL,
+  `image` MEDIUMBLOB NULL,
+  `fkSupplier` INT(11) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`productId`),
+  FOREIGN KEY (`fkSupplier`) REFERENCES `supplier`(`supplierId`) ON DELETE CASCADE ON UPDATE CASCADE
+)
+
+CREATE TABLE `user` (
+  `userId` INT(11) NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(32) NOT NULL,
+  `password` VARCHAR(64) NOT NULL,
+  `confirmPassword` VARCHAR(64) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`userId`)
+)
